ef8d2033b87c88c711895f01f6c056cdbb641a8f
Rizky Muslimin <55826921+Rizky-Muslimin@users.noreply.github.com>
2019-10-17 21:05:59 +0700

    Add files via upload

---
+++ PBO1-10118002-Latihan39-(ObjekOriented)NilaiTerbesardanTerkecil/src/pbo1/pkg10118002/latihan39/objekoriented/nilaiterbesardanterkecil/PBO110118002Latihan39ObjekOrientedNilaiTerbesardanTerkecil.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118002.latihan39.objekoriented.nilaiterbesardanterkecil;
+import java.util.Scanner;
+
+/**
+ *
+ * @author 
+ * NAMA     : Rizky Muslimin
+ * KELAS    : IF-1
+ * NIM      : 10118002
+ * Deskripsi Program : Program ini untuk menampilkan tampilan nilai terbesar dan 
+ *                     terkecil.
+ */
+public class PBO110118002Latihan39ObjekOrientedNilaiTerbesardanTerkecil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Mahasiswa mhs1 = new Mahasiswa();
+        Scanner scn = new Scanner(System.in);
+        
+        int bykMah;
+        String nama;
+        System.out.println
+        ("========Program Nilai Terbesar dan Terkecil nilai Mahasiswa========");
+        System.out.print("Masukkan Nama Petugas : ");
+        nama = scn.nextLine();
+        System.out.print("Masukkan Banyaknya Mahasiswa : ");
+        bykMah = scn.nextInt();
+        mhs1.inputNilai(mhs1.nilai, bykMah);
+        mhs1.tampilBesarKecil(mhs1.nilai, bykMah);
+        
+        System.out.println("\n"+"Petugas : " + nama);
+
+    }
+    
+}
+
+
